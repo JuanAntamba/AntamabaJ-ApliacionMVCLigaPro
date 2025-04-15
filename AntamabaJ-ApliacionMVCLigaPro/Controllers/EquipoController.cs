da179aef04688e75fb2c6c72489ce8f916ec1790
Juan Carlos Antamba <juan.antamba@udla.edu.ec>
2025-04-14 23:57:55 -0500

    Creación repositorio

--- AntamabaJ-ApliacionMVCLigaPro/Controllers/EquipoController.cs
+++ AntamabaJ-ApliacionMVCLigaPro/Controllers/EquipoController.cs
@@ -1,4 +1,5 @@
 ﻿using AntamabaJ_ApliacionMVCLigaPro.Models;
+using AntamabaJ_ApliacionMVCLigaPro.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AntamabaJ_ApliacionMVCLigaPro.Controllers
@@ -7,64 +8,10 @@ namespace AntamabaJ_ApliacionMVCLigaPro.Controllers
     {
         public IActionResult ListaEquipos()
         {
-            List<Equipo> equipos = new List<Equipo>();
+            EquipoRepository equipoRepository = new EquipoRepository();
+            var equipos = equipoRepository.DevuelveListadoEquipos();
 
-            Equipo ldu = new Equipo
-            {
-                Id = 1,
-                Nombre = "LDU",
-                PatidosJugados = 20,
-                PartidosGanados = 15,
-                PartidosEmpatados = 5,
-                PartidosPerdidos =0
-            };
-            equipos.Add(ldu);
-
-            Equipo bsc = new Equipo
-            {
-                Id = 2,
-                Nombre = "Barcelona SC",
-                PatidosJugados = 20,
-                PartidosGanados = 10,
-                PartidosEmpatados = 5,
-                PartidosPerdidos = 5
-            };
-            equipos.Add(bsc);
-
-            Equipo nacional = new Equipo
-            {
-                Id = 3,
-                Nombre = "Nacional",
-                PatidosJugados = 20,
-                PartidosGanados = 9,
-                PartidosEmpatados = 9,
-                PartidosPerdidos = 2
-            };
-            equipos.Add(nacional);
-
-            Equipo emelec= new Equipo
-            {
-                Id = 4,
-                Nombre = "Emelec",
-                PatidosJugados = 20,
-                PartidosGanados = 1,
-                PartidosEmpatados = 11,
-                PartidosPerdidos = 8
-            };
-            equipos.Add(emelec);
-
-            Equipo idv = new Equipo
-            {
-                Id = 5,
-                Nombre = "Independiente del Valle",
-                PatidosJugados = 20,
-                PartidosGanados = 5,
-                PartidosEmpatados = 10,
-                PartidosPerdidos = 5
-            };
-            equipos.Add(idv);
-
-            return View(equipos);
+            return View(equipos);   
         }
     }
 }
---
+++ AntamabaJ-ApliacionMVCLigaPro/Repositories/EquipoRepository.cs
@@ -0,0 +1,64 @@
+﻿using AntamabaJ_ApliacionMVCLigaPro.Models;
+
+namespace AntamabaJ_ApliacionMVCLigaPro.Repositories
+{
+    public class EquipoRepository
+    {
+        public List<Equipo> DevuelveListadoEquipos()
+        {
+            List<Equipo> equipos = new List<Equipo>();
+            Equipo ldu = new Equipo
+            {
+                Id = 1,
+                Nombre = "LDU",
+                PatidosJugados = 20,
+                PartidosGanados = 15,
+                PartidosEmpatados = 5,
+                PartidosPerdidos = 0
+            };
+            equipos.Add(ldu);
+            Equipo bsc = new Equipo
+            {
+                Id = 2,
+                Nombre = "Barcelona SC",
+                PatidosJugados = 20,
+                PartidosGanados = 10,
+                PartidosEmpatados = 5,
+                PartidosPerdidos = 5
+            };
+            equipos.Add(bsc);
+            Equipo nacional = new Equipo
+            {
+                Id = 3,
+                Nombre = "Nacional",
+                PatidosJugados = 20,
+                PartidosGanados = 9,
+                PartidosEmpatados = 9,
+                PartidosPerdidos = 2
+            };
+            equipos.Add(nacional);
+            Equipo emelec = new Equipo
+            {
+                Id = 4,
+                Nombre = "Emelec",
+                PatidosJugados = 20,
+                PartidosGanados = 1,
+                PartidosEmpatados = 11,
+                PartidosPerdidos = 8
+            };
+            equipos.Add(emelec);
+            Equipo idv = new Equipo
+            {
+                Id = 5,
+                Nombre = "Independiente del Valle",
+                PatidosJugados = 20,
+                PartidosGanados = 12,
+                PartidosEmpatados = 6,
+                PartidosPerdidos = 2
+            };
+            equipos.Add(idv);
+
+            return equipos;
+        }
+    }
+}
