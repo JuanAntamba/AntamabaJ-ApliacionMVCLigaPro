f92a9936771f88c25edc7cabb576b2bcdf7ae5a4
Juan Carlos Antamba <juan.antamba@udla.edu.ec>
2025-04-14 22:49:25 -0500

    Layout tabla de posiciones

---
+++ AntamabaJ-ApliacionMVCLigaPro/Controllers/EquipoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AntamabaJ_ApliacionMVCLigaPro.Controllers
+{
+    public class EquipoController : Controller
+    {
+        public IActionResult ListaEquipos()
+        {
+            return View();
+        }
+    }
+}
